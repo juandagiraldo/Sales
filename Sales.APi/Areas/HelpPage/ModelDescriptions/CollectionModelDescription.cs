2413b81937104c56459bec3d5192b19fc306a74f
juandagiraldo <juanchito--18@hotmail.com>
2018-12-17 15:13:01 -0500

    1. Project Creation
    1. Creación del proyecto Sales (Ventas), de la base de datos en Azure y modificación de la clase IdentityConfig.cs para que al momento de ingresar una contraseña cuando se registre un usuario no pida un digito, una minuscula, una mayuscula y un caracter alfanumérico.

---
+++ Sales.APi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Sales.APi.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Sales.APi/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Sales.APi.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
